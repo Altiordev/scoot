18046b954b4901f115ee6597219ebd581a865004
Altiordev <akbarxon2005@gmail.com>
2023-02-18 20:39:22 +0500

    fixed-bug

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 const hamburger = document.querySelector(".mobil-menu");
 const navMenu = document.querySelector(".menu");
-const body = document.querySelector('body');
+// const body = document.querySelector('body');
 
 
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
-    body.classList.toggle('lock');
+    // body.classList.toggle('lock');
 
 
 })
